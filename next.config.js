b6fe146f5d040f5383b5d5d7505d7ef57a754199
Ruben Bautista Mendoza <oxigeno78@gmail.com>
2025-10-29 20:10:20 -0600

    CORS...

--- next.config.js
+++ next.config.js
@@ -41,6 +41,18 @@ const nextConfig = {
             key: 'Referrer-Policy',
             value: 'origin-when-cross-origin',
           },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          }
         ],
       },
     ];
